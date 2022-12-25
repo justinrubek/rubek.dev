5b4a982cf865f1f634859963b65bfd83596c1221
Justin Rubek <25621857+justinrubek@users.noreply.github.com>
2022-12-25 04:32:33 -0600

    feat: add server start script

--- default.nix
+++ default.nix
@@ -117,6 +117,9 @@
         mkdir -p $out/bin
         cp ${api-package}/bin/api $out/bin
       '';
+      server_script = pkgs.writeShellScriptBin "start_server" ''
+        ${api-package}/bin/api ${self'.packages.site}
+      '';
     };
 
     apps = {
