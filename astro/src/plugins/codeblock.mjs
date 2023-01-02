eeb208f0eabd774c466374a6248e39862cf6269b
Justin Rubek <25621857+justinrubek@users.noreply.github.com>
2023-01-01 19:04:44 -0600

    feat: display extra data on codeblocks

---
+++ astro/src/plugins/codeblock.mjs
@@ -0,0 +1,26 @@
+import { visit } from "unist-util-visit";
+
+export default function codeBlockData(options = {}) {
+    return (tree) => {
+        visit(tree, (node) => {
+            if (node.type == "html" && node.value.includes('class="astro-code"')) {
+                // include the language as a data attribute
+                let lang = node.lang ?? "";
+                if (lang != "") {
+                    lang = `data-lang="${lang}"`;
+                }
+
+                // include other key/value pairs as data attributes
+                const data_attrs = String(node.meta).replace(/(\w+\=.*?)/g, "data-$1");
+
+                const meta_string = `
+                    ${lang} ${data_attrs}
+                `;
+                node.value = node.value.replace(
+                    '"astro-code"',
+                    (item) => `${item} ${meta_string}`
+                );
+            }
+        });
+    };
+}
