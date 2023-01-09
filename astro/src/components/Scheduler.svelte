8d4e853696ef1517f151530b83251ef232e9f534
Justin Rubek <25621857+justinrubek@users.noreply.github.com>
2023-01-09 00:43:34 -0600

    feat: use forked svelte-calendar

--- astro/src/components/Scheduler.svelte
+++ astro/src/components/Scheduler.svelte
@@ -1,11 +1,62 @@
 <script lang="ts">
     import { onMount } from 'svelte'
+    import { Datepicker, themes } from '@justinrubek/svelte-calendar'
+    import dayjs from 'dayjs'
 
+    const { dark: theme } = themes;
+    theme.calendar.width = '500px';
+    // theme.calendar.maxWidth = '20vw';
+
+    const today = new Date();
+    const tomorrow = new Date(today);
+    tomorrow.setDate(tomorrow.getDate() + 1);
+    const lastweek = new Date(today);
+    lastweek.setDate(lastweek.getDate() - 7);
+
+    const markedDates = [today, tomorrow, lastweek];
     let schedule_availability;
+
+    function process_availability(availability) {
+        const { start, end, matrix } = availability;
+        // matrix - a list of booleans representing 30 minute blocks
+        // start - a string representing the start time of the time range for matrix
+        // end - a string representing the end time of the time range for matrix
+        // granularity (not in this example) - a string representing the granularity of the matrix (in seconds). Assume it is 30 minutes
+        const granularity = 30 * 60;
+
+        // The data needs to mapped to a calendar
+        // To do this we should iterate over days in the local timezone
+        // and produce an object indexed by the date that corresponds to the time
+        // range for that day 
+        // ultimately, we want something like this (assume this is psuedo code) and variables should be stored as relevant JavaScript types
+        /* 
+        start = "2023-01-01T00:00:00Z"
+        end = "2023-01-31T23:59:59Z"
+        // Note that the month is indexed from 0, but the day is indexed from 1
+        availableDates = { 
+            "2023": {
+                "0": [ 10, 11, 15, 16 ]
+            },
+        }
+        availability = {
+            "2023" {
+                "0": {
+                    "10": [ "2023-01-01T10:00:00Z", "2023-01-01T10:30:00Z" ],
+                    "11": [ "2023-01-01T11:00:00Z", "2023-01-01T11:30:00Z" ],
+                    "15": [ "2023-01-01T15:00:00Z", "2023-01-01T15:30:00Z" ],
+                    "16": [ "2023-01-01T16:00:00Z", "2023-01-01T16:30:00Z" ],
+                }
+            }
+        };
+
+        */
+
+    }
 
     onMount(() => {
         console.log('Scheduler mounted')
         let start = new Date();
+        /*
         fetch('http://localhost:8000/availability', {
             method: 'POST',
             cache: 'no-cache',
@@ -23,5 +74,7 @@
         }).then(() => {
             console.log(schedule_availability);
         });
+        */
     })
 </script>
+<Datepicker {markedDates} {theme}/>
--- crates/api/src/main.rs
+++ crates/api/src/main.rs
@@ -1,9 +1,14 @@
 use axum::{
     http::StatusCode,
     response::IntoResponse,
-    routing::{get, get_service},
+    routing::{get, get_service, post},
     Router,
 };
+use caldav_utils::{
+    availability::get_availability,
+    caldav::client::{DavClient, DavCredentials},
+};
+use scheduling_api::{get_calendars, get_now, request_availability, state::CaldavAvailability};
 use std::net::SocketAddr;
 use tower_http::services::ServeDir;
 
@@ -18,9 +23,40 @@ async fn main() {
         .and_then(|it| it.parse().ok())
         .unwrap_or(8000);
 
+    let username = std::env::var("CALDAV_USERNAME").expect("CALDAV_USERNAME not set");
+    let password = std::env::var("CALDAV_PASSWORD").expect("CALDAV_PASSWORD not set");
+    let credentials = DavCredentials::new(username.to_string(), password.to_string());
+
+    let url = std::env::var("CALDAV_URL").expect("CALDAV_URL not set");
+
+    let dav_client = DavClient::new(url.to_string(), credentials);
+
+    let availability_calendar =
+        std::env::var("AVAILABLE_CALENDAR").expect("AVAILABLE_CALENDAR not set");
+    let booked_calendar = std::env::var("BOOKED_CALENDAR").expect("BOOKED_CALENDAR not set");
+
+    let caldav_state = CaldavAvailability::new(
+        availability_calendar.to_string(),
+        booked_calendar.to_string(),
+        dav_client,
+    );
+
+    scheduler_api(caldav_state, dir).await.unwrap();
+}
+
+async fn scheduler_api(caldav_state: CaldavAvailability, dir: String) -> Result<(), Box<dyn std::error::Error>> {
+    let port = std::env::var("PORT")
+        .ok()
+        .and_then(|it| it.parse().ok())
+        .unwrap_or(8000);
+
     let serve_dir = get_service(ServeDir::new(dir)).handle_error(handle_error);
     let app = Router::new()
+        .route("/now", get(get_now))
+        // POST since JS doesn't support body in GET
+        .route("/availability", post(request_availability))
         .route("/health", get(health))
+        .with_state(caldav_state)
         .fallback_service(serve_dir);
 
     let addr = SocketAddr::from(([0, 0, 0, 0], port));
@@ -29,7 +65,10 @@ async fn main() {
         .serve(app.into_make_service())
         .await
         .unwrap();
+
+    Ok(())
 }
+
 
 async fn health() -> &'static str {
     "OK"
