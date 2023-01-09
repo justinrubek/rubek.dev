642b944c488dd1606e4a126b6817cf390ce1d81a
Justin Rubek <25621857+justinrubek@users.noreply.github.com>
2023-01-09 16:42:18 -0600

    feat: selected day shows available times for meeting

---
+++ astro/src/components/scheduling/Timepicker.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+    import { getContext } from 'svelte'
+
+    import { contextKey } from './store';
+    import Timecard from './Timecard.svelte';
+
+    const scheduleStore = getContext(contextKey);
+
+    export let selected;
+    export let selectedTimeslots;
+
+    export let selectTime = (start, end) => {
+        console.log('selectTime', start, end);
+    }
+</script>
+{#if selectedTimeslots.length > 0}
+    <ul>
+        {#each selectedTimeslots as timeslot}
+            <Timecard {selectTime} start={timeslot[0]} end={timeslot[1]} />
+        {/each}
+    </ul>
+{:else}
+    <p>No timeslots available on this day</p>
+{/if}
--- astro/src/components/scheduling/types.ts
+++ astro/src/components/scheduling/types.ts
@@ -2,8 +2,8 @@ import type { Dayjs } from 'dayjs'
 
 // { year: { month: { day: [start_time, end_time] } } }
 export type TimeRange = [Dayjs, Dayjs];
-type Day = Record<string, TimeRange[]>;
-type Month = Record<string, Day>;
-type Year = Record<string, Month>;
+export type Day = Record<string, TimeRange[]>;
+export type Month = Record<string, Day>;
+export type Year = Record<string, Month>;
 
 export type CalendarAvailability = Year;
