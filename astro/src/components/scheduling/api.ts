fd5a84e87061bfad56e0ecf14210d9311005d78f
Justin Rubek <25621857+justinrubek@users.noreply.github.com>
2023-01-13 12:16:49 -0600

    feat(site): make api url configurable

--- astro/src/components/scheduling/api.ts
+++ astro/src/components/scheduling/api.ts
@@ -1,4 +1,12 @@
 import wretch from 'wretch'
 
-// TODO: API_BASE_URL should be configurable
-export const api = wretch('http://localhost:8000');
+function get_api_url() {
+    // Check if PUBLIC_SCHEDULE_API_URL is set, if not default to the current host
+    if (import.meta.env.PUBLIC_SCHEDULE_API_URL) {
+        return import.meta.env.PUBLIC_SCHEDULE_API_URL
+    }
+
+    return ""
+
+}
+export const api = wretch(get_api_url())
